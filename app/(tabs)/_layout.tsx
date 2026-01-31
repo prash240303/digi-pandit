daedca36bb2edd7909ab28741103e336312be3c8
prashant <prash2402@gmail.com>
2026-02-01 03:36:54 +0530

    feat : inital setup, UI library, add bottom nav

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,17 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Tabs } from "expo-router";
+import React from "react";
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { HapticTab } from "@/components/haptic-tab";
+import { Colors } from "@/constants/theme";
+import { useColorScheme } from "@/hooks/use-color-scheme";
+import { HugeiconsIcon } from "@hugeicons/react-native";
+import {
+  AlphabetHindiIcon,
+  Calendar01FreeIcons,
+  HandPrayerFreeIcons,
+  Home11FreeIcons,
+  MapsLocation02FreeIcons,
+} from "@hugeicons/core-free-icons";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -12,22 +19,54 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         headerShown: false,
         tabBarButton: HapticTab,
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <HugeiconsIcon icon={Home11FreeIcons} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="calendar"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: "Calendar",
+          tabBarIcon: ({ color }) => (
+            <HugeiconsIcon icon={Calendar01FreeIcons} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="mantras"
+        options={{
+          title: "Mantras",
+          tabBarIcon: ({ color }) => (
+            <HugeiconsIcon icon={AlphabetHindiIcon} color={color} />
+          ),
+        }}
+      />{" "}
+      <Tabs.Screen
+        name="rituals"
+        options={{
+          title: "Rituals",
+          tabBarIcon: ({ color }) => (
+            <HugeiconsIcon icon={HandPrayerFreeIcons} color={color} />
+          ),
+        }}
+      />{" "}
+      <Tabs.Screen
+        name="temples"
+        options={{
+          title: "Temples",
+          tabBarIcon: ({ color }) => (
+            <HugeiconsIcon icon={MapsLocation02FreeIcons} color={color} />
+          ),
         }}
       />
     </Tabs>
